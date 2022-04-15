13e297773bf3798dcb7100cdf3c4241a964103cc
SteveL <stevelyne1971@gmail.com>
2022-04-15 09:59:23 +0200

    edit item tweaked

--- src/app/admin/edit-item/edit-item.component.ts
+++ src/app/admin/edit-item/edit-item.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
-import { DataStorageService } from 'src/app/shared/data-storage.service';
+
 import { Item } from 'src/app/shared/item.model';
+import { DataStorageService } from 'src/app/shared/data-storage.service';
 import { ShoppingListService } from 'src/app/shopping-list.service';
 
 @Component({
@@ -10,37 +10,24 @@ import { ShoppingListService } from 'src/app/shopping-list.service';
   templateUrl: './edit-item.component.html',
   styleUrls: ['./edit-item.component.css']
 })
+
 export class EditItemComponent implements OnInit {
-  @ViewChild('f2') itemForm!: NgForm;
+
+  @ViewChild('f') itemForm!: NgForm;
   defaultItem: string = this.shoppingListService.getItems()[0].name;
- 
-  chosenItem = <Item>{ };
-  
-  chosenItemName: string = "";
-
-  selectedValue: string ="";
-
-  uniqueCategories: string[] = [];
-  defaultCategory: string = "Wall";
-  chosenCategory: string = "";
-  collapse:boolean=true;
-  
-  source:string = "items"
-  items: Item[] = [];
   submitted = false;
+  itemEdited=false;
+  itemDeleted=false;
+  chosenItem = <Item>{ };
+  chosenItemName: string = "";
+  selectedValue: string ="";
+  collapse:boolean=true;
+  defaultCategory: string = "";
+  items: Item[] = [];
   names: string[] = [];
-
-  item: Item = {
-    id: '',
-    category: '',
-    name: '',
-    description: '',
-    imageUrl: '',
-    price: 0,
-  }
+  uniqueCategories: string[] = [];
 
   constructor(private shoppingListService: ShoppingListService,
-              private router:Router,
               private dataStorageService: DataStorageService) { }
 
   ngOnInit(): void {
@@ -49,44 +36,41 @@ export class EditItemComponent implements OnInit {
     this.uniqueCategories = [...new Set(this.items.map(item => item.category))];
   }
 
-  // onSubmit1(form : NgForm) {
   onChange(chosenItem : string) {
     this.chosenItemName = chosenItem;
     const found = this.items.findIndex(element => element.name === this.chosenItemName );
     this.chosenItem = this.items[found]
     this.loadChosenItem(this.chosenItem)
     this.collapse=false;
-
   }
 
-  onSubmit2() {
+  onSubmit() {
+    const item = new Item(
+      this.chosenItem.id,
+      this.itemForm.value.itemData.category,
+      this.itemForm.value.itemData.name,
+      this.itemForm.value.itemData.description,
+      this.itemForm.value.itemData.imageUrl,
+      this.itemForm.value.itemData.price
+    );
+    this.shoppingListService.updateItem(item);
+    this.dataStorageService.storeItems();
+    
+    this.submitted=true;
+    this.itemEdited=true;
+    this.resetForms();
+  }
+
+  onDeleteItem() {
+    this.shoppingListService.deleteItem(this.chosenItem.id);
+    this.dataStorageService.storeItems();
 
     this.submitted=true;
-
-    this.item.id = this.chosenItem.id;
-    this.item.category = this.itemForm.value.itemData.category;
-    this.item.name = this.itemForm.value.itemData.name;
-    this.item.description = this.itemForm.value.itemData.description;
-    this.item.imageUrl = this.itemForm.value.itemData.imageUrl;
-    this.item.price = this.itemForm.value.itemData.price;
-  
-    this.shoppingListService.updateItem(this.item);
-    this.dataStorageService.storeItems();
-    this.resetForms();
-    this.collapse=true;
-  }
-
-  exitEdit() {
-    this.itemForm.reset();
-    this.collapse=true;
-  }
-
-  onResetFilter() {
-    this.chosenItem
+    this.itemDeleted=true;
+    this.resetForms(); 
   }
 
   loadChosenItem(item: Item) {
-
     this.itemForm.setValue({
       itemData: {
         category: item.category,
@@ -98,20 +82,14 @@ export class EditItemComponent implements OnInit {
     });
   }
 
-  onDeleteItem() {
-    this.shoppingListService.deleteItem(this.item.id)
-    this.dataStorageService.storeItems();
-    this.resetForms(); 
-  }
-
   resetForms() {
     this.itemForm.reset();
     this.items = this.shoppingListService.getItems();
     this.names = [...new Set(this.items.map(item => item.name))];
     this.uniqueCategories = [...new Set(this.items.map(item => item.category))];
     this.defaultItem= this.shoppingListService.getItems()[0].name;
-
     this.collapse=true;
+    this.selectedValue="";
   }
 
 }
